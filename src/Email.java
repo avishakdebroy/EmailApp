cf06a9dd7fa781c5dabfa798fb8811d6d6cb5773
Avishak Deb Roy <95048039+avishakdebroy@users.noreply.github.com>
2022-11-07 15:50:55 -0500

    update

--- src/Email.java
+++ src/Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -10,7 +12,27 @@ public class Email {
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.department = setDepartment();
         System.out.println("EMAIL CREATED: " + this.firstName +" " + this.lastName);
+    }
+
+    // Method
+    private String setDepartment() {
+        System.out.println("Enter the department\n1 for Sales\n2 for Devolopment\n3 for Accounting\n0 for None");
+        Scanner scanner = new Scanner(System.in);
+        int deptChoice = scanner.nextInt();
+        if (deptChoice == 1) {
+            return "sales";
+        }
+        else if (deptChoice == 2) {
+            return "dev";
+        }
+        else if (deptChoice == 3) {
+            return "acct";
+        }
+        else {
+            return "";
+        }
     }
 
 }
